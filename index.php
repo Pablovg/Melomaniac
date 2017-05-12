2abded0a59d1fc5088f8ad870a290f845418dfc9
Pablovg <pablovgdev@gmail.com>
2017-05-12 13:25:37 +0200

    starting the web

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php session_start(); ?>
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="general.css" type="text/css" rel="stylesheet" />
+    <link href="index.css" type="text/css" rel="stylesheet" />
+    <title>Melomaniac</title>
+</head>
+<body>
+
+    <nav>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+    </nav>
+
+    <div class="">
+
+    </div>
+
+</body>
+</html>
