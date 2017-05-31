4667e0685aa24eeae8d17e1c42ff8a391815c455
Pablovg <pablovgdev@gmail.com>
2017-05-31 11:23:53 +0200

    Terminado

--- DB/melomaniac.sql
+++ DB/melomaniac.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 30-05-2017 a las 23:45:48
+-- Tiempo de generación: 31-05-2017 a las 11:08:39
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 7.1.2
 
